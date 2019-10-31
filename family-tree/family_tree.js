726f8fb80f627b45798a4ca34a5d375cc0cf79b4
jphayek <jphayek1@gmail.com>
2019-10-31 15:19:47 +0100

    find_and_give_ascendance

--- family-tree/family_tree.js
+++ family-tree/family_tree.js
@@ -1,26 +1,35 @@
 // STORE family as an array
-var family = {
-	Jack: { name: 'Jack', age: 90, sex: 'm', parent: null },
-	Olivier: { name: 'Olivier', age: 60, sex: 'm', parent: 'Jack' },
-	Pascal: { name: 'Pascal', age: 60, sex: 'm', parent: 'Jack' },
-	Angélique: { name: 'Angélique', age: 18, sex: 'f', parent: 'Olivier' },
-	Charlotte: { name: 'Charlotte', age: 24, sex: 'f', parent: 'Olivier' },
-	Julien: { name: 'Julien', age: 34, sex: 'm', parent: 'Pascal' },
-	Caroline: { name: 'Caroline', age: 31, sex: 'f', parent: 'Pascal' },
-	Barnabé: { name: 'Barnabé', age: 2, sex: 'm', parent: 'Julien' },
-	Constance: { name: 'Constance', age: 0, sex: 'f', parent: 'Caroline' },
-	Jeff: { name: 'Jeff', age: -2, sex: 'm', parent: 'Caroline' }
-};
+var family = [
+	{ name: 'Jack', age: 90, sex: 'm', parent: null },
+	{ name: 'Olivier', age: 60, sex: 'm', parent: 'Jack' },
+	{ name: 'Pascal', age: 60, sex: 'm', parent: 'Jack' },
+	{ name: 'Angélique', age: 18, sex: 'f', parent: 'Olivier' },
+	{ name: 'Charlotte', age: 24, sex: 'f', parent: 'Olivier' },
+	{ name: 'Julien', age: 34, sex: 'm', parent: 'Pascal' },
+	{ name: 'Caroline', age: 31, sex: 'f', parent: 'Pascal' },
+	{ name: 'Barnabé', age: 2, sex: 'm', parent: 'Julien' },
+	{ name: 'Constance', age: 0, sex: 'f', parent: 'Caroline' },
+	{ name: 'Jeff', age: -2, sex: 'm', parent: 'Caroline' }
+];
+
+function find_person(family_tree, name) {
+	for (p in family_tree) {
+		if (family_tree[p].name == name) {
+			return family_tree[p];
+		}
+	}
+	return false;
+}
 
 function give_ascendance(family_tree, name) {
 	var ascendance = [];
 	do {
-		var person = family_tree[name];
+		var person = find_person(family_tree, name);
 		var parent_name = person.parent; //"Pascal"
 
 		if (parent_name === null) break;
 
-		var parent = family_tree[parent_name];
+		var parent = find_person(family_tree, parent_name);
 		ascendance.push(parent);
 		name = parent.name;
 	} while (parent !== null);
@@ -31,7 +40,7 @@ function give_ascendance(family_tree, name) {
 var asc = give_ascendance(family, 'Caroline');
 console.log(asc);
 
-// store family as a real tree
+// AS TREE
 var family_as_tree = {
 	name: 'Jack',
 	age: 90,
@@ -71,6 +80,7 @@ var family_as_tree = {
 	]
 };
 
+//HELPER CLASS
 function Tree(rootNode) {
 	this._root = rootNode;
 
@@ -85,3 +95,22 @@ function Tree(rootNode) {
 }
 
 var zamor = new Tree(family_as_tree);
+
+function find_and_give_ascendance(family_as_tree, search) {
+	var result = [];
+
+	family_as_tree.traverse(function(person, ascendance) {
+		console.log('i');
+		if (person.name == search) {
+			result = [ person ].concat(ascendance);
+		}
+	});
+
+	return result.map(function(p) {
+		return { name: p.name };
+	});
+}
+
+var asc = find_and_give_ascendance(zamor, 'Barnabé');
+console.log(asc);
+//[{"name": "Barnabé"},{"name": "Julien"},{"name": "Pascal"}, {"name": "Jack"}]
